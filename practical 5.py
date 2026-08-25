bc8d1848912c945a0b43f83a2bce26d7b8203ef1
Shreyas020pol <shreyasvpol@gmail.com>
2026-08-25 13:32:13 +0530

    Delete practical 5.py

--- practical 5.py
+++
@@ -1,8 +0,0 @@
-print("*****************Monthly Expenses Tracker*****************")
-Expenses = 0.0
-while True:
-    Value = float(input("Enter your Amount: "))
-    if Value == -1:
-        break
-    Expenses = Expenses + Value
-    print("Total Expenses:", Expenses)
